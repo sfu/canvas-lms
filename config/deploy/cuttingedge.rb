fb954785248c1b2eb4ea62611916da0f6525c5e0
Graham Ballantyne <grahamb@sfu.ca>
2016-03-17 13:17:11 -0700

    put edge symlink task into the canvas namespace

--- config/deploy/cuttingedge.rb
+++ config/deploy/cuttingedge.rb
@@ -14,9 +14,11 @@ set :default_env, {
   'PATH' => '/usr/pgsql-9.1/bin:$PATH'
 }
 
-desc "Create symlink for files folder to mount point"
-task :symlink_canvasfiles do
-  on roles(:all) do
-    execute "ln -s #{shared_path}/tmp/files #{release_path}/tmp/files"
+namespace :canvas do
+  desc "Create symlink for files folder to mount point"
+  task :symlink_canvasfiles do
+    on roles(:all) do
+      execute "ln -s #{shared_path}/tmp/files #{release_path}/tmp/files"
+    end
   end
 end
