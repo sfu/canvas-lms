dc2858231e7109dd14165436460104e2cddd69b3
Graham Ballantyne <grahamb@sfu.ca>
2016-11-22 15:25:26 -0800

    disable sfu_help

--- gems/plugins/sfu_help/lib/sfu_help.rb
+++ gems/plugins/sfu_help/lib/sfu_help.rb
@@ -1,15 +1,15 @@
-module SFU
-  module Help
-    class Railtie < ::Rails::Railtie
-      initializer "sfu_help.prepend_module" do |app|
-        Account::HelpLinks.singleton_class.prepend(SFU::Help)
-      end
-    end
-    def default_links
-      []
-    end
-    def self.prepended(klass)
-      Rails.logger.info("****** prepended #{self} to #{klass}") unless Rails.env.production?
-    end
-  end
-end
+# module SFU
+#   module Help
+#     class Railtie < ::Rails::Railtie
+#       initializer "sfu_help.prepend_module" do |app|
+#         Account::HelpLinks.singleton_class.prepend(SFU::Help)
+#       end
+#     end
+#     def default_links
+#       []
+#     end
+#     def self.prepended(klass)
+#       Rails.logger.info("****** prepended #{self} to #{klass}") unless Rails.env.production?
+#     end
+#   end
+# end
