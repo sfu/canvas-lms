1675569935fc7e8a8c56dda965857f1a545eab2e
Graham Ballantyne <hello@grahamballantyne.com>
2016-11-22 14:52:26 -0800

    Update sfu_help plugin to use Account::HelpLinks
    `Canvas::Help` was changed to `Account::HelpLinks` in 49f867a

--- gems/plugins/sfu_help/lib/sfu_help.rb
+++ gems/plugins/sfu_help/lib/sfu_help.rb
@@ -2,7 +2,7 @@ module SFU
   module Help
     class Railtie < ::Rails::Railtie
       initializer "sfu_help.prepend_module" do |app|
-        Canvas::Help.singleton_class.prepend(SFU::Help)
+        Account::HelpLinks.singleton_class.prepend(SFU::Help)
       end
     end
     def default_links
