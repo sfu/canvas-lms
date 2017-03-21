5c59cb6e5e49f4b4beb314da71acd04dfa548d94
Graham Ballantyne <hello@grahamballantyne.com>
2017-03-21 13:57:15 -0700

    Convert class_type to string before downcasing
    `class_type` should already be a string, but up on L54 it has `.to_s` called on it, so let's do it here for consistency.

--- gems/plugins/sfu_api/app/model/sfu/sfu.rb
+++ gems/plugins/sfu_api/app/model/sfu/sfu.rb
@@ -81,8 +81,8 @@ module SFU
         if details.present? && is_enrollment_section?(details, section)
           associated_class = associated_class_for_section(details, section)
           details.each do |info|
-            class_type = info["course"]["classType"]
-            if class_type.downcase.eql?("n") && associated_class == info["course"]["associatedClass"]
+            class_type = info["course"]["classType"].to_s.downcase
+            if class_type.eql?("n") && associated_class == info["course"]["associatedClass"]
               sections << info["course"]["section"]
             end
           end
