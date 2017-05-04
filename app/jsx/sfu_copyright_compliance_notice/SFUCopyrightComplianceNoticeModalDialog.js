dece5352f5e10e383362fac0e20021bd43ce35f3
Graham Ballantyne <grahamb@sfu.ca>
2017-05-04 15:59:16 -0700

    Convert SFUCopyrightComplianceNoticeModalDialog to a class

--- app/jsx/sfu_copyright_compliance_notice/SFUCopyrightComplianceNoticeModalDialog.js
+++ app/jsx/sfu_copyright_compliance_notice/SFUCopyrightComplianceNoticeModalDialog.js
@@ -1,89 +1,92 @@
-define([
-  'react',
-  'react-modal',
-  './SFUCopyrightComplianceNotice'
-  ], function (React, ReactModal, SFUCopyrightComplianceNotice) {
+import React from 'react'
+import ReactModal from 'react-modal'
+import SFUCopyrightComplianceNotice from './SFUCopyrightComplianceNotice'
 
-    var SFUCopyrightComplianceModalDialog = React.createClass({
+class SFUCopyrightComplianceModalDialog extends React.Component {
 
-      propTypes: {
-        modalIsOpen: React.PropTypes.bool
-      },
+  constructor (props) {
+    super(props)
+    this.state = {
+      modalIsOpen: this.props.modalIsOpen
+    }
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+    this.publishCourse = this.publishCourse.bind(this)
+  }
 
-      getInitialState() {
-        return {
-          modalIsOpen: this.props.modalIsOpen
-        }
-      },
-
-      componentWillReceiveProps: function(nextProps) {
-        this.setState({
-          modalIsOpen: nextProps.modalIsOpen
-        });
-      },
-
-      openModal(e) {
-        e.preventDefault();
-        this.setState({modalIsOpen: true});
-      },
-
-      closeModal(e) {
-        if (e) {
-          e.preventDefault();
-        }
-        this.setState({
-          modalIsOpen: false
-        });
-      },
-
-      publishCourse(e) {
-        if (e) {
-          e.preventDefault();
-        }
-        this.setState({
-          modalIsOpen: false
-        }, () => {
-          document.getElementById(this.props.formId).submit();
-        });
-      },
-
-      render() {
-        return(
-          <ReactModal
-            isOpen={this.state.modalIsOpen}
-            className='ReactModal__Content--canvas'
-            overlayClassName='ReactModal__Overlay--canvas'
-          >
-            <div className="ReactModal__Layout">
-
-              <div className="ReactModal__InnerSection ReactModal__Header">
-                <div className="ReactModal__Header-Title">
-                  <h4>Copyright Compliance Notice</h4>
-                </div>
-                <div className="ReactModal__Header-Actions">
-                  <button className="Button Button--icon-action" type="button" onClick={this.closeModal}>
-                    <i className="icon-x"></i>
-                    <span className="screenreader-only">Close</span>
-                  </button>
-                </div>
-              </div>
-
-              <div className="ReactModal__InnerSection ReactModal__Body">
-                <SFUCopyrightComplianceNotice />
-              </div>
-
-              <div className="ReactModal__InnerSection ReactModal__Footer">
-                <div className="ReactModal__Footer-Actions">
-                  <button type="button" className="btn btn-default" onClick={this.closeModal}>Cancel</button>
-                  <button type="button" className="btn btn-primary" onClick={this.publishCourse}>Publish</button>
-                </div>
-              </div>
-
-            </div>
-          </ReactModal>
-        )
-      }
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      modalIsOpen: nextProps.modalIsOpen
     });
+  }
 
-  return SFUCopyrightComplianceModalDialog;
-});
+  openModal (e) {
+    e.preventDefault();
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal (e) {
+    if (e) {
+      e.preventDefault();
+    }
+    this.setState({
+      modalIsOpen: false
+    });
+  }
+
+  publishCourse (e) {
+    if (e) {
+      e.preventDefault();
+    }
+    this.setState({
+      modalIsOpen: false
+    }, () => {
+      document.getElementById(this.props.formId).submit();
+    });
+  }
+
+  render () {
+    return (
+      <ReactModal
+        contentLabel="Copyright Compliance Notice"
+        isOpen={this.state.modalIsOpen}
+        className="ReactModal__Content--canvas"
+        overlayClassName="ReactModal__Overlay--canvas"
+      >
+        <div className="ReactModal__Layout">
+
+          <div className="ReactModal__InnerSection ReactModal__Header">
+            <div className="ReactModal__Header-Title">
+              <h4>Copyright Compliance Notice</h4>
+            </div>
+            <div className="ReactModal__Header-Actions">
+              <button className="Button Button--icon-action" type="button" onClick={this.closeModal}>
+                <i className="icon-x" />
+                <span className="screenreader-only">Close</span>
+              </button>
+            </div>
+          </div>
+
+          <div className="ReactModal__InnerSection ReactModal__Body">
+            <SFUCopyrightComplianceNotice />
+          </div>
+
+          <div className="ReactModal__InnerSection ReactModal__Footer">
+            <div className="ReactModal__Footer-Actions">
+              <button type="button" className="btn btn-default" onClick={this.closeModal}>Cancel</button>
+              <button type="button" className="btn btn-primary" onClick={this.publishCourse}>Publish</button>
+            </div>
+          </div>
+
+        </div>
+      </ReactModal>
+    )
+  }
+}
+
+SFUCopyrightComplianceModalDialog.propTypes = {
+  modalIsOpen: React.PropTypes.bool.isRequired,
+  formId: React.PropTypes.string.isRequired
+}
+
+export default SFUCopyrightComplianceModalDialog
