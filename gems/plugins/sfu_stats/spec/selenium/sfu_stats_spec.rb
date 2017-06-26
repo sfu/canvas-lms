dbe3c56125445b41c303e58ec602f4ba867efbd3
Graham Ballantyne <grahamb@sfu.ca>
2017-06-26 11:02:39 -0700

    spec: sfu_stats: initial stats spec commit

---
+++ gems/plugins/sfu_stats/spec/selenium/sfu_stats_spec.rb
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2017 - present Simon Fraser University.
+#
+# This file is part of Simon Fraser University's fork of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+require File.expand_path(File.dirname(__FILE__) + '../../../../../../spec/selenium/common')
+require File.expand_path(File.dirname(__FILE__) + '../../../../../../spec/selenium/helpers/sfu_common')
+
+describe "SFU Stats Page" do
+  include_context "in-process server selenium tests"
+  include SFUCommon
+
+  STATS_PAGE = '/sfu/stats'
+
+  before :once do
+    create_sfu_terms
+  end
+
+  it "should load the page" do
+    user_session(user_with_pseudonym(:name => "Test User", :active_user => true, :username => "testuser@example.com", :account => Account.default))
+    get STATS_PAGE
+    expect(driver.title).to eq 'Canvas Stats'
+  end
+
+end
