eebed58a29a8b978b0b2d159ed0e9aa4b7e0d87c
Patrick Chin <patchin@sfu.ca>
2017-08-11 15:36:33 -0700

    Restore jqueryUI widget functionality lost with recent Canvas changes. Taken from: 'https://github.com/ryankshaw/widgetize-canvas-lms-user-content'

--- public/sfu/js/sfu.js
+++ public/sfu/js/sfu.js
@@ -6,6 +6,10 @@
 
  */
 
+!function(s,d,url,e,p){
+  e=d.createElement(s),p=d.getElementsByTagName(s)[0];e.async=1;e.src=url;p.parentNode.insertBefore(e,p)
+}('script', document, 'https://unpkg.com/widgetize-canvas-lms-user-content');
+
 (function ($) {
   var utils = {
     onPage (regex, fn) {
