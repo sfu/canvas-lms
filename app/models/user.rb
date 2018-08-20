5cb679f0f505a8fa8e0727ab0b2b0341b7d27041
Cody Cutrer <cody@instructure.com>
2018-08-20 11:04:56 -0600

    open source has no weak or shadow shard associations
    fixes gh-1166
    
    Change-Id: I02da60c386a3587334c53cf69e9bccb101c36378
    Reviewed-on: https://gerrit.instructure.com/161334
    Reviewed-by: James Williams  <jamesw@instructure.com>
    Tested-by: Jenkins
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2668,7 +2668,7 @@ class User < ActiveRecord::Base
   end
 
   def associated_shards(strength = :strong)
-    [Shard.default]
+    strength == :strong ? [Shard.default] : []
   end
 
   def in_region_associated_shards
