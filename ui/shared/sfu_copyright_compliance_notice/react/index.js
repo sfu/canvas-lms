b20f36a5f5ced4a0d1ac077f61bd560dfee66b26
Graham Ballantyne <grahamb@sfu.ca>
2021-05-06 16:05:49 -0700

    Create @sfu/sfu-copyright-compliance-notice package

---
+++ ui/shared/sfu_copyright_compliance_notice/react/index.js
@@ -0,0 +1,9 @@
+import SFUCopyrightComplianceNotice from './SFUCopyrightComplianceNotice';
+import SFUCopyrightComplianceNoticeModalDialog from './SFUCopyrightComplianceNoticeModalDialog';
+import SFUCopyrightComplianceNoticeMoreInfo from './SFUCopyrightComplianceNoticeMoreInfo';
+
+export {
+  SFUCopyrightComplianceNotice,
+  SFUCopyrightComplianceNoticeModalDialog,
+  SFUCopyrightComplianceNoticeMoreInfo,
+};
